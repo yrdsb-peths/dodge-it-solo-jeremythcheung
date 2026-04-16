68d8a35950d5282fcfc1cb6088b088f7e6c2516b
jeremythcheung <jeremythcheung@gmail.com>
2026-04-16 18:49:36 -0400

    Set smaller world size for Jeremy's dodge-it arena

--- MyWorld.java
+++ MyWorld.java
@@ -2,6 +2,10 @@ import greenfoot.*;
 
 public class MyWorld extends World {
     public MyWorld() {
-        super(600, 400, 1);
+        // Jeremy's world: smaller arena for dodge-it
+        super(500, 300, 1);
+
+        Hero hero = new Hero();
+        addObject(hero, 50, 50);
     }
 }
